f6ecb10dd4766f9dd2ccca707b174a452e5aec6a
unknown <imamkusdinar@gmail.com>
2024-09-03 18:43:47 +0700

    update

--- src/components/Portofolio.jsx
+++ src/components/Portofolio.jsx
@@ -1,10 +1,5 @@
+import { portofolioList } from "../data/DataPortofolio"
 
-import portoFolio1 from "../assets/portfolio1.png"
-import portoFolio2 from "../assets/portfolio2.png"
-import portoFolio3 from "../assets/portfolio3.png"
-import portoFolio4 from "../assets/portfolio4.png"
-import portoFolio5 from "../assets/portfolio5.png"
-import portoFolio6 from "../assets/portfolio6.png"
 
 function Portofolio() {
   return (
@@ -12,15 +7,16 @@ function Portofolio() {
         <div className="wrapper">
             <h3>Portofolio</h3>
             <div className="grid">
-                <div className="item">
-                    <a href="#"><img src={portoFolio1} /></a>
-                    <a href="#"><img src={portoFolio2} /></a>
-                    <a href="#"><img src={portoFolio3} /></a>
-                    <a href="#"><img src={portoFolio4} /></a>
-                    <a href="#"><img src={portoFolio5} /></a>
-                    <a href="#"><img src={portoFolio6} /></a>
-
-                </div>
+               {
+                portofolioList.map((item)=>{
+                  return(
+                    <div className="item" key={item.id}>
+                      <a href=""><img src={item.image} /></a>
+                  </div>
+                  )
+                })
+               }
+               
             </div>
         </div>
 
---
+++ src/data/DataPortofolio.jsx
@@ -0,0 +1,38 @@
+import portofolio1 from '../assets/portfolio1.png'
+import portofolio2 from '../assets/portfolio2.png'
+import portofolio3 from '../assets/portfolio3.png'
+import portofolio4 from '../assets/portfolio4.png'
+import portofolio5 from '../assets/portfolio5.png'
+import portofolio6 from '../assets/portfolio6.png'
+
+export const portofolioList = [
+    {
+        id:'1',
+        image:portofolio1
+    },
+
+    {
+        id:'2',
+        image:portofolio2
+    },
+
+    {
+        id:'3',
+        image:portofolio3
+    },
+
+    {
+        id:'4',
+        image:portofolio4
+    },
+
+    {
+        id:'5',
+        image:portofolio5
+    },
+
+    {
+        id:'6',
+        image:portofolio6
+    },
+]

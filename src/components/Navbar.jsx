55826cc0d269a923c8cd8159985b5c6ccef05f5b
unknown <imamkusdinar@gmail.com>
2024-09-03 09:57:12 +0700

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
 
 function Navbar() {
+    const[statusTampil, setStatusTampil] = useState('')
+    function tampilMenu(){
+        if (statusTampil == '' ){
+            setStatusTampil('tampil')
+        } else {
+        setStatusTampil('')
+        }
+    }
     return (
       <nav>
           <div className="wrapper">
               <div className="logo">
                   <a href="#">My Portofolio</a>
               </div>
-              <div className="menu">
+              <button onClick={tampilMenu}><FaBars /></button>
+              <div className={`menu ${statusTampil}`}>
                   <ul><li><a href="#">Portofolio</a></li>
                   <li><a href="#">About</a></li>
                   <li><a href="#">Experience</a></li>

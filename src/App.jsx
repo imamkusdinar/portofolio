dd4d2ca94a47a05b2c1e9397e73f70da77f8009d
unknown <imamkusdinar@gmail.com>
2024-09-03 15:17:51 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import './App.css'
+import './Navbar.css'
+import Home from './pages/Home.jsx'
 
 export default function App() {
   return (
-    <div>Test App</div>
+   <>
+    <Home />
+ 
+ </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+
+function Navbar() {
+    return (
+      <nav>
+          <div className="wrapper">
+              <div className="logo">
+                  <a href="#">My Portofolio</a>
+              </div>
+              <div className="menu">
+                  <ul><li><a href="#">Portofolio</a></li>
+                  <li><a href="#">About</a></li>
+                  <li><a href="#">Experience</a></li>
+                  </ul>
+              </div>
+          </div>
+          </nav>
+    )
+  }
+  
+  export default Navbar

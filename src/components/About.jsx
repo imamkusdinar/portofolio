173527a0410d84ccb42175e58ae3a30a61218bde
unknown <imamkusdinar@gmail.com>
2024-09-05 17:50:23 +0700

    change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,20 +17,12 @@ function About() {
     <section id='about'>
         <div className='wrapper'>
             <h3>About</h3>
-            <p>Saya adalah seorang programmer berpengalaman dengan latar belakang yang kuat dalam pengembangan perangkat lunak dan pemecahan masalah teknis,</p>
-            <p>saya memiliki keterampilan yang luas dalam berbagai bahasa pemrograman dan teknologi. Saya telah berkontribusi pada berbagai proyek, mulai dari aplikasi web dan mobile hingga sistem berbasis cloud dan perangkat lunak desktop.</p>
-            <p>Dengan keahlian dalam Python, JavaScript, Java, C++, saya mampu merancang dan mengimplementasikan solusi yang efisien dan scalable. Pengalaman saya dalam React, Django, Node.js, AWS memungkinkan saya untuk membangun aplikasi yang responsif 
-            dan dapat diandalkan.</p>
-            <p>Saya sangat terampil dalam analisis kebutuhan pengguna, perancangan arsitektur sistem, dan pengujian perangkat lunak. Selain itu, saya memiliki keterampilan dalam bekerja dalam tim lintas disiplin dan berkomunikasi dengan jelas</p>
-            <p>dengan berbagai pemangku kepentingan untuk memastikan proyek memenuhi tujuan dan tenggat waktu.</p>
-            Keahlian saya meliputi:
-            <p>Pengembangan Web: Membuat dan mengelola aplikasi web menggunakan HTML, CSS, JavaScript, dan framework terkait.
-            Pengembangan Aplikasi Mobile: Mengembangkan aplikasi mobile untuk platform Android dengan Visual Studio Code dan Android Studio.
-            Database Management: Mengelola dan mengoptimalkan database menggunakan  MySQL, FireBase .
-            DevOps: Mengimplementasikan praktik DevOps untuk otomatisasi dan pengelolaan infrastruktur menggunakan Docker, Kubernetes.
-            <p>Pemecahan Masalah: Mengidentifikasi dan menyelesaikan bug serta masalah performa untuk memastikan aplikasi berjalan dengan lancar.
-            Saya selalu mencari tantangan baru dan kesempatan untuk mengembangkan keterampilan saya lebih lanjut. Dengan semangat untuk teknologi dan inovasi,</p>
-            <p>saya berkomitmen untuk memberikan solusi yang memenuhi dan melampaui ekspektasi klien dan pengguna.</p> </p>        
+            <p>Saya adalah seorang programmer berpengalaman dengan keahlian pemrograman PHP, Python, JavaScript, Java, Laravel, ReactJS, MySQL. 
+            Saya ahli dalam membangun aplikasi web dan mobile yang efisien serta solusi perangkat lunak yang scalable.
+            Saya berpengalaman dengan berbagai teknologi. 
+            Terampil dalam analisis masalah, pengembangan kode yang bersih,
+            dan bekerja dalam tim untuk mencapai tujuan proyek. 
+            Saya selalu bersemangat untuk setiap tantangan baru dan inovasi teknologi.</p>      
             <h4>Programming Language</h4>
             <div className='skills'>
             <FaHtml5 /><FaPhp /><FaLaravel /><RiReactjsFill /><SiMysql /><FaCss3Alt /><DiJavascript /><FaPython /><SiArduino />
